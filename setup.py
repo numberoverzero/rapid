6917766a8d59310cbc2e1d8c1bf48ad2fe0b635a
Joe Cross <joe.mcross@gmail.com>
2017-09-17 01:09:23 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "requirements.txt")) as f:
+    requirements = f.readlines()
+
+
+if __name__ == "__main__":
+    setup(
+        name="rapid",
+        version="0",
+        author="Joe Cross",
+        author_email="joe.mcross@gmail.com",
+        url="https://github.com/numberoverzero/rapid",
+        license="MIT",
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=requirements,
+    )
