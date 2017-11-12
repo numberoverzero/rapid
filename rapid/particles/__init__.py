584b1bbdb50171b3cae0cfd197ae5f9a4b9dcd3b
Joe Cross <joe.mcross@gmail.com>
2017-11-11 17:05:52 -0800

    First particle impl, skeleton.Game doesn't require host/port to start

---
+++ rapid/particles/__init__.py
@@ -0,0 +1,187 @@
+import pyglet.gl
+from pyglet.graphics import Batch
+from pyglet.graphics.vertexdomain import VertexList
+
+
+from typing import Optional, Tuple, TypeVar
+from ..scene import BatchDrawable
+
+__all__ = [
+    "LineParticleCollection"
+]
+
+Color = Tuple[int, int, int, int]  # c4B
+Vec2 = Tuple[float, float]  # v2f
+T = TypeVar("T")
+
+
+def monotonic_counter(initial: int=0):
+    value = initial
+    while True:
+        yield value
+        value += 1
+
+
+GLOBAL_ID = monotonic_counter()
+
+
+# noinspection PyProtectedMember
+class LineParticleCollection(BatchDrawable):
+    """Collection of particles, rendered using GL_LINES"""
+    active_particles: int
+    max_particles: int
+
+    _inactive_head: Optional["LineParticle"]
+    _active_head: Optional["LineParticle"]
+
+    _last_rendered_count: int
+    verts: VertexList
+    dirty: bool  # set to True to re-stream vert/color data
+
+    def __init__(self, max_particles: int=1000, batch: Optional[Batch]=None) -> None:
+        super().__init__(batch)
+        self.verts = self.batch.add(2 * max_particles, pyglet.gl.GL_LINES, None, "v2f", "c4B")
+        # self.verts = self.batch.add(max_particles, pyglet.gl.GL_LINES, None, ('v2f', (0.0,) * 200), ('c4B', (255,) * 400))
+
+        self.dirty = True
+
+        self._last_rendered_count = max_particles
+        self.active_particles = 0
+        self.max_particles = max_particles
+
+        self._active_head = None
+        self._inactive_head = None
+        for _ in range(max_particles):
+            # _prev_particle isn't set, because we only need to track prev when free-ing an active particle
+            particle = LineParticle(collection=self)
+            particle._next_particle = self._inactive_head
+            self._inactive_head = particle
+
+    def alloc(self) -> Optional["LineParticle"]:
+        """Pull an inactive particle off the inactive stack, push it onto the active stack"""
+        particle = self._inactive_head
+        if particle is None:
+            return None
+
+        self.dirty = True
+
+        # Pop the to-be-returned particle from the inactive stack
+        self._inactive_head = particle._next_particle
+
+        # Push the to-be-returned particle onto the active stack
+        particle._next_particle = old_head = self._active_head
+        if old_head is not None:
+            old_head._prev_particle = particle
+        self._active_head = particle
+        particle._prev_particle = None
+
+        self.active_particles += 1
+
+        particle._id = next(GLOBAL_ID)
+        return particle
+
+    def free(self, particle: "LineParticle") -> None:
+        # TODO | is this actually a problem?  Double free should simply re-order
+        # TODO | the inactive list, without actually losing a particle...
+        assert particle._id is not None, "tried to double free"
+        particle._id = None
+        self.dirty = True
+
+        prev = particle._prev_particle
+        next = particle._next_particle
+
+        if particle is self._active_head:
+            self._active_head = next
+
+        # Pointers to this particle now point to each other: A <--> particle <--> B becomes A <--> B
+        if prev is not None:
+            prev._next_particle = next
+        if next is not None:
+            next._prev_particle = prev
+
+        # Clear this particle's pointers, and push in onto the head of the inactive stack.
+        # _prev is always None on the inactive stack since we only need a singly linked list to allocate
+        particle._prev_particle = None
+        particle._next_particle = self._inactive_head
+        self._inactive_head = particle
+
+        self.active_particles -= 1
+
+    def on_update(self, dt: float) -> None:
+        if self.dirty:
+            new_verts = [0] * 4 * self.active_particles  # Each particle has 2 (x, y) pairs
+            new_colors = [0] * 8 * self.active_particles  # Each particle has a (r, g, b, a) tuple
+
+            particle = self._active_head
+            for i in range(self.active_particles):
+                new_verts[i * 4: i * 4 + 4] = [*particle.p0, *particle.p1]
+                new_colors[i * 8: i * 8 + 8] = particle.color * 2
+                particle = next(particle)
+            assert particle is None, "excluded some active particles"
+
+            if self._last_rendered_count != self.active_particles:
+                self.verts.resize(2 * self.active_particles)
+                self._last_rendered_count = self.active_particles
+            try:
+                self.verts.vertices = new_verts
+            except ValueError:
+                print(f"new_verts: {len(new_verts)} vertices: {len(self.verts.vertices)} active: {self.active_particles}")
+                raise
+
+            try:
+                self.verts.colors = new_colors
+            except ValueError:
+                print(f"new_colors: {len(new_colors)} colors: {len(self.verts.colors)} active: {self.active_particles}")
+                raise
+
+            self.dirty = False
+
+
+# noinspection PyProtectedMember
+class ParticleProperty:
+    # noinspection PyTypeChecker
+    def __get__(self, instance: Optional["LineParticle"], owner):
+        if instance is None:
+            return self
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance: "LineParticle", value):
+        instance.__dict__[self.name] = value
+        instance._collection.dirty = True
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+
+class LineParticle:
+    _collection: LineParticleCollection
+    _prev_particle: Optional["LineParticle"]
+    _next_particle: Optional["LineParticle"]
+    _id: Optional[int]
+
+    p0: Vec2 = ParticleProperty()
+    p1: Vec2 = ParticleProperty()
+    color: Color = ParticleProperty()
+
+    def __init__(self, collection: LineParticleCollection) -> None:
+        self._collection = collection
+        self._prev_particle = None
+        self._next_particle = None
+        self._id = None
+
+        self.p0 = 0, 0
+        self.p1 = 0, 0
+        self.color = 255, 255, 255, 255
+
+    def __repr__(self) -> str:
+        if self._id is not None:
+            x = f"{self._id}, {id(self)}"
+        else:
+            x = id(self)
+        return f"<LineParticle[{x}]>"
+
+    def __next__(self) -> Optional["LineParticle"]:
+        return self._next_particle
+
+    def free(self) -> None:
+        self._collection.free(self)
--- skeleton/game.py
+++ skeleton/game.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from rapid import Scene, Camera, Window
 from skeleton.net import Client
 
@@ -5,14 +6,18 @@ from skeleton.net import Client
 class Game(Scene):
     def __init__(
             self,
-            host: str, port: int, mode: str="tcp",
+            host: Optional[str]=None, port: Optional[int]=None, mode: str="tcp",
             screen_width=1024, screen_height=768, title="<GAME TITLE HERE>"):
         camera = Camera()
         super().__init__(camera, name=title)
         window = Window(width=screen_width, height=screen_height)
         window.add_scene(self)
-        self.client = Client(self, host=host, port=port, mode=mode)
+        if host is not None and port is not None:
+            self.client = Client(self, host=host, port=port, mode=mode)
+        else:
+            self.client = None
 
     def run(self):
-        self.client.connect()
+        if self.client:
+            self.client.connect()
         self.window.run()
