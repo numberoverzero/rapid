8a995b98a6b4a8014849d195195242df6aa9cf34
Joe Cross <joe.mcross@gmail.com>
2017-11-18 22:10:27 -0800

    expose rapid.Vec2 directly

--- rapid/__init__.py
+++ rapid/__init__.py
@@ -1,11 +1,12 @@
+from .util import Sentinel, Vec2
 from .windowing import Camera, Scene, Window
-from .util import Sentinel
+
 
 from pyglet.window import key
 
 __all__ = [
     "Camera", "Scene", "Window",
-    "Sentinel",
+    "Sentinel", "Vec2",
 
     # repackaged
     "key"
